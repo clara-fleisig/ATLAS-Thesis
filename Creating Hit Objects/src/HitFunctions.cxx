15903d9327cff88dea7f5fe7f2e3b99cbadfd6da
cfleisig <clara.fleisig@cern.ch>
2024-05-04 15:48:22 +0200

    added construct + creating vector of Hit objs

--- Creating Hit Objects/src/HitFunctions.cxx
+++ Creating Hit Objects/src/HitFunctions.cxx
@@ -1,14 +1,27 @@
 #include <vector>
-#include "HitFunctions.h"
 #include <cstdint>
 #include <TFile.h>
 #include <TTree.h>
 #include <iostream> 
+#include <vector>
+#include "HitFunctions.h"
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <chrono>
+
+bool CheckValue(ROOT::Internal::TTreeReaderValueBase& value) {
+   if (value.GetSetupStatus() < 0) {
+      std::cerr << "Error " << value.GetSetupStatus()
+                << "setting up reader for " << value.GetBranchName() << '\n';
+      return false;
+   }
+   return true;
+}
 
 Hit_org::Hit create_hit_obj(std::int64_t i){
-    //get point to tree
+    //get pointer to tree
     std::unique_ptr<TFile> myFile(TFile::Open("src/ClusterHitSeedRoot.root"));
-    auto hitInfoTree = myFile->Get<TTree>("HitInfo");
+    TTree *hitInfoTree = myFile->Get<TTree>("HitInfo");
     if(hitInfoTree == nullptr){
         std::cerr << "TTree::HitInfo does not exist in ClusterHitSeedRoot.root" << std::endl;
         exit(-1);
@@ -24,20 +37,66 @@ Hit_org::Hit create_hit_obj(std::int64_t i){
     hitInfoTree -> GetEntry(0);
 
     //create hit_info object
-    Hit_org::Hit my_hit;
-    my_hit.x = x->at(i);
-    my_hit.y = y->at(i);
-    my_hit.z = z->at(i);
+    Hit_org::Hit my_hit(x->at(i), y->at(i), z->at(i));
+    //my_hit.x = x->at(i);
+    //my_hit.y = y->at(i);
+    //my_hit.z = z->at(i);
 
     return my_hit;
 }
 
+std::vector<Hit_org::Hit> create_hit_vec(TFile *File){
+    //read branches of interest into vecotrs of type std::vector<float>
+    TTreeReader reader("HitInfo", File);
+    TTreeReaderValue<std::vector<float>> x(reader, "hit_x");
+    TTreeReaderValue<std::vector<float>> y(reader, "hit_y");
+    TTreeReaderValue<std::vector<float>> z(reader, "hit_z");
+    reader.Next();
+
+    //check if branches are there
+    if (!CheckValue(x) && !CheckValue(y) && !CheckValue(z)) exit(-1);
+    
+    //setup for itterating through vectors
+    std::vector<Hit_org::Hit> hit_vec;
+    auto x_it = x -> begin();
+    auto y_it = y -> begin();
+    auto z_it = z -> begin();
+    auto x_end = x -> end();
+
+    //itterate over vectors to create objects and push them
+    while(x_it < x_end){
+        //create object and add to hit_vec
+        Hit_org::Hit cur_hit(*x_it, *y_it, *z_it);
+        /*cur_hit.x = *x_it;
+        cur_hit.y = *y_it;
+        cur_hit.z = *z_it;*/
+        hit_vec.push_back (cur_hit);
+
+        //iterate through vectors
+        x_it++; y_it++; z_it++;
+    }
+
+    //error catching
+    if ((x_it != x->end()) || (y_it != y -> end()) || (z_it != z -> end())){
+        std::cerr << "Itteration through branches was unsuccessful" << std::endl;
+        exit(-1);
+    }
+
+    return hit_vec;
+}
+
 int main()
 {
-    std::cout<<"Hello world"<<std::endl;
-    
-    Hit_org::Hit hit_0 = create_hit_obj(0);
-    std::cout<<"x: "<< hit_0.x <<std::endl;
+    TFile *File = TFile::Open("src/ClusterHitSeedRoot.root");
+
+    std::vector<Hit_org::Hit> hit_vec = create_hit_vec(File);
+    std::cout << "Hit Vector Size: " << hit_vec.size() << '\n';
+
+    //auto t1 = high_resolution_clock::now();
+    //auto t2 = high_resolution_clock::now();
+
+    Hit_org::Hit myhit = create_hit_obj(0);
+    myhit.show_Hit();
 
     return 0;
 }
--- Creating Hit Objects/src/HitFunctions.h
+++ Creating Hit Objects/src/HitFunctions.h
@@ -1,13 +1,28 @@
 #pragma once
 #include <cstdint>
+#include <TTree.h>
 
 namespace Hit_org{
     class Hit{
         public:
+            //attributes
             float x;
             float y;
             float z;
+
+            // Constructor with parameters
+            Hit(float my_x, float my_y, float my_z) {
+                x = my_x;
+                y = my_y;
+                z = my_z;
+            }
+
+            void show_Hit()
+            {
+                std::cout << "x: " << x << "\t" << "y: " << y << "\t" << "z: " << z << "\t" << std::endl;
+            }
     };
 
     Hit_org::Hit create_hit_obj(std::int64_t i);
+    std::vector<Hit_org::Hit> create_hit_vec(TFile *File);
 }
